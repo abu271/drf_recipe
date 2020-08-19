89eb0217bc6e61462fa11da2b8388a48f02dfaf5
abuDarda97 <abudarda166@gmail.com>
2020-08-19 22:02:54 +0100

    Create unit test for auth token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -6,6 +6,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse("user:create")
+TOKEN_URL = reverse("user:token")
 
 
 def create_user(**params):
@@ -59,3 +60,47 @@ class PublicUserApiTests(TestCase):
         ).exists()
 
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """Test that a token is made for user"""
+        payload = {
+            "email": "test@outlook.com",
+            "password": "test123",
+            "name": "test"
+        }
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn("token", res.data)
+        self.assertEqaul(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_with_invalid_creds(self):
+        """Test token is not created when passed invalid creds"""
+        create_user(email="test@outlook.com", password="test123")
+        payload = {
+            "email": "test@outlook.com",
+            "password": "wrong"
+        }
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn("token", res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_no_user(self):
+        """Test token is not created when user doesn't exist"""
+        payload = {
+            "email": "test@outlook.com",
+            "password": "test123"
+        }
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn("token", res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_fields(self):
+        """Test token is not created when fields are missing"""
+        res = self.client.post(
+            TOKEN_URL, {"email": "test@outlook.com", "password": ""})
+
+        self.assertNotIn("token", res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
