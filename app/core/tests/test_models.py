30ca5219951460e31e023a7213f2c2dff6b53ade
abuDarda97 <abudarda166@gmail.com>
2020-08-29 14:16:12 +0100

    Create recipe model
    * add recipe model
    * create recipe model unit test
    * register recipe model to admin

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -64,3 +64,14 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title="Choclate cake",
+            time_minutes=30,
+            price=6.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
