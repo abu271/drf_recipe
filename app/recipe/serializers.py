f94c3bb44dccb57dd93191239ec3620ecf0bea09
abuDarda97 <abudarda166@gmail.com>
2020-08-31 17:40:47 +0100

    Add custom detail view for recipe
    * add unit test for detail view
    * create serializer for detail view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -40,3 +40,9 @@ class RecipeSerializer(serializers.ModelSerializer):
             "price", "link"
         )
         read_only_fields = ("id",)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a detail recipe"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -43,3 +43,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Get the recipes for the authenticated user"""
         return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        """Return correct serializer class"""
+        if self.action == "retrieve":
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class
